e7a7e831cdbce8bc6adc9abcda28ae0eab8f35cc
Thaddäus Schima <t.schima@mailbox.org>
2023-02-07 00:24:27 +0100

    Update ContentView.swift

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @FocusState private var inputTempIsFocused: Bool
+    @State private var inputTemperatur = 0.0
+    private var convertedValue: Double {
+        if inputTempType == "ºC" {
+            if outputTemperaturType == "F" {
+                return inputTemperatur * 1.8 + 32
+            } else if outputTemperaturType == "K" {
+                return inputTemperatur - 270.0
+            } else {
+                return inputTemperatur
+            }
+        } else {
+            return inputTemperatur
         }
-        .padding()
+        
+    }
+    @State private var outputTemperaturType = "F"
+    @State private var inputTempType:String = "ºC"
+    let conversionTemperatureTypes = ["ºC", "F", "K"]
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Your Temperatur: ", value: $inputTemperatur, format: .number).focused($inputTempIsFocused)
+                        .keyboardType(.decimalPad)
+                    Picker("Choose the temp type", selection: $inputTempType) {
+                        ForEach(conversionTemperatureTypes, id: \.self) {
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("Input your Temperatur and the type")
+                }
+                Section {
+                    Picker("Choose your output Temp type", selection: $outputTemperaturType) {
+                        ForEach(conversionTemperatureTypes, id: \.self) {
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("Choose your output Temperatur")
+                }
+                Section {
+                    Text((convertedValue), format: .number)
+                } header: {
+                    Text("Output Temperatur")
+                }
+            }.navigationTitle("Temperatur Converter")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        
+                        Button("Done") {
+                            inputTempIsFocused = false
+                        }
+                    }
+                }
+        }
     }
 }
 
