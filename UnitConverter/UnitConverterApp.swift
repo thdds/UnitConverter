46afce28f49f5126a459dec5d9d3cbea4e0f25d0
Thaddäus Schima <t.schima@mailbox.org>
2023-02-06 23:19:42 +0100

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Thaddäus Schima on 06.02.23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
